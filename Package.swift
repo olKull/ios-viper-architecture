8a844aec3f3005feef3650de9f71c50467356c98
Oleh Kulakevych <kor.geloer@gmail.com>
2025-04-30 14:53:07 +0300

    Update SPM

--- Package.swift
+++ Package.swift
@@ -1,16 +1,22 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "ViperArch",
     platforms: [
-        .iOS(.v11),
-        .tvOS(.v11),
+        .iOS(.v13)
     ],
     products: [
-        .library(name: "ViperArch", targets: ["ViperArch"]),
+        .library(
+            name: "ViperArch",
+            targets: ["ViperArch"]
+        )
     ],
     targets: [
-        .target(name: "ViperArch", dependencies: [], path: "Source"),
+        .binaryTarget(
+            name: "ViperArch",
+            url: "https://github.com/yourname/ViperArch/releases/download/1.0.4/ViperArch.xcframework.zip",
+            checksum: "00db241a423af8527253c7791b35bd11c0f7bfe9d4ddf281b94b61427203e6c2"
+        )
     ]
 )
